9a3e619a4a52d4fcd21a114a9f32c35da1329817
Nikku <yoks.rar@gmail.com>
2025-06-19 18:07:59 -0400

    Correcion del PWA

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', calculateResults);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/calculadora/service-worker.js')
       .then(registration => {
         console.log('Service Worker registrado con éxito:', registration);
       })
